05a1711500e5cf4ee70e7fcaaf6bbb5c48e93fb8
Prithviraj Kumar <65295955+prithvirajkumar48@users.noreply.github.com>
2020-10-18 19:02:37 +0530

    Create index.js

---
+++ DAY-6 ASSIGNMENT/script/index.js
@@ -0,0 +1,9 @@
+// alert("Click me First!!");
+console.log("Hey You!");
+var customerName = prompt("Please enter your name");
+
+if (customerName!= null) {
+
+  document.getElementById("welcome").innerHTML =  "Hello " + customerName + "! How are you today?";
+
+}
